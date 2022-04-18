13921968cc371ba4c6146df61f2b64c05851bc20
omnireader0 <omnireader0@gmail.com>
2022-04-18 18:55:09 +0900

    h2 -> mysql db 변경

--- playlist/src/main/java/play/playlist/PlaylistApplication.java
+++ playlist/src/main/java/play/playlist/PlaylistApplication.java
@@ -3,9 +3,9 @@ package play.playlist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
+@EnableAutoConfiguration
 @SpringBootApplication
 public class PlaylistApplication {
 
