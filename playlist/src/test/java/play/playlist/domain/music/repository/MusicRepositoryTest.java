3925d5f5d3349e3d3a7953e4e9ebfc882021df8d
omnireader0 <omnireader0@gmail.com>
2022-05-06 21:09:30 +0900

    test : 음악 정보 (전체 조회, 상세 조회) 테스트 작성

---
+++ playlist/src/test/java/play/playlist/domain/music/repository/MusicRepositoryTest.java
@@ -0,0 +1,47 @@
+package play.playlist.domain.music.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+import play.playlist.domain.music.dao.MusicRepository;
+import play.playlist.domain.music.dto.MusicResponseDto;
+import play.playlist.domain.music.entity.Music;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+
+
+@ActiveProfiles("test")
+@SpringBootTest
+public class MusicRepositoryTest {
+
+    @Autowired
+    MusicRepository musicRepository;
+
+    @Test
+    public void get_all_musicInfo() {
+
+        // when
+        List<Music> music = musicRepository.findAll();
+
+        // then
+        assertThat(music, hasSize(600));
+    }
+
+    @Test
+    public void get_one_musicInfo() {
+
+        // when
+        Music one = musicRepository.findById(5L).orElseThrow(() -> new
+                IllegalArgumentException("해당 정보는 없습니다."));
+
+        // then
+        assertThat(one.getArtist()).isEqualTo("아이유");
+        assertThat(one.getSongName()).isEqualTo("금요일에 만나요 (Feat. 장이정 Of HISTORY)");
+    }
+}
